727fbc0573d4ba23a3027b4c4f9ceddc423868cd
Rob Hislop <rhislop@pandora.com>
2016-04-27 12:39:49 -0700

    project and package cleanup

--- pyhoofinance/__init__.py
+++ pyhoofinance/__init__.py
@@ -2,7 +2,7 @@
 #
 # pyhoofinance - quotedata.py
 #
-# Copyright (c) 2014, Rob Innes Hislop
+# Copyright (c) 2014-2016, Rob Innes Hislop
 # email:robinneshislop__AT__gmail.com
 #
 # This library is distributed under the terms of the 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 #  pyhoofinance - setup.py
 #
-#  Copyright (c) 2014-2015, Rob Innes Hislop
+#  Copyright (c) 2014-2016, Rob Innes Hislop
 #  email:robinneshislop__AT__gmail.com
 #
 # This library is distributed under the terms of the 
@@ -11,7 +11,7 @@
    
 from setuptools import setup, find_packages
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 setup(
     name='pyhoofinance',
     version=__version__,
